14f21cb47ddbc03d85e258878efe8abe18aafa55
dsapozhnikov <31924660+dsapozhnikov@users.noreply.github.com>
2017-11-17 19:33:30 +0300

    внесение векторов

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -6,95 +6,116 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 
 
 import javax.swing.*;
-import java.util.Random;
 
+
+/*
+Создаем игру простую  для отбивания мяча битой
+ */
 
 public class MyGdxGame extends ApplicationAdapter {
-    SpriteBatch batch;
-    Texture img2;
-    static class Bita {
-		float x,y;
-		float vx, vy;
+	SpriteBatch batch;
+
+
+	static class Bita {
+		private Vector2 position;
+		private float speed;
+		private float width;
+		private float height;
 		Texture texture;
 		float angle;
 
-		public Bita(Texture texture, float x, float y, float vx, float vy ) {
-			this.texture = texture;
-			this.x = x;
-			this.y = y;
-			this.vx = vx;
-			this.vy = vy;
-			this.angle=135;
-		}
-		public void render(SpriteBatch batch) {
+		public Bita() {                                         // создаем Биту для отбивания мяча
 
-			batch.draw(texture,x,y,160,160,500,500,1,1,angle,0,0,640,640,true,false
+			this.texture = new Texture("bita.png");    // назначаем параметры биты
+			this.position = new Vector2(640, -70);
+			this.speed = 500f;
+			this.width = 340;
+			this.height = 100;
+			this.angle = 135;
+		}
+
+		public void render(SpriteBatch batch) {                // отрисовываем биту
+
+			batch.draw(texture, position.x, position.y, 160, 160, 500, 500, 1, 1, angle, 0, 0, 640, 640, true, false
 			);
 		}
+
 		public void update(float dt) {
+
+			if (Gdx.input.isKeyPressed(Input.Keys.A)) {       // управление битой , при нажатии "А" бита движется влево
+				position.x -= speed * dt;
+			}
+			if (Gdx.input.isKeyPressed(Input.Keys.D)) {        // управление битой , при нажатии "D" бита движется вправо
+				position.x += speed * dt;
+			}
 
 		}
 	}
+                                                            // создаем  класс мяча
+	static class Obj {
+		private float radius;
+		private Vector2 position;
+		private Vector2 velocity;
+		Texture texture;
+		float angle;
 
-	static class Obj{
-	    float x,y;
-	    float vx, vy;
-	    Texture texture;
-        float angle;
+		public Obj() {
+			this.position = new Vector2(640, 640);            //  задаем параметры мяча
+			this.velocity = new Vector2(400f, 2000f);        // скорости перемещения мяча по x и y
+			this.radius = 160;                                   // зааем радиус для отладки отскока от стены
+			this.texture = new Texture("miyach.png");
 
+			this.angle = 0;
 
-        public Obj(Texture texture, float x, float y, float vx, float vy) {
-            this.texture = texture;
-            this.x = x;
-            this.y = y;
-            this.vx = vx;
-            this.vy = vy;
-            this.angle = 0;
+		}
 
-        }
+		public void render(SpriteBatch batch) {        // отрисовываем мяч
 
-        public void render(SpriteBatch batch) {
+			batch.draw(texture, position.x-radius, position.y-radius, radius, radius, radius * 2, radius * 2, 1, 1, angle, 0, 0, 320, 320, false, false);
 
-                batch.draw(texture, x-160, y-160,160,160,320,320,1,1,angle,0,0,320,320,false,false);
+		}
 
-        }
+		public void update(float dt) {
 
-        public void update(float dt) {
+			position.mulAdd(velocity, dt);           // задаем условия отбивания мяча от стенок верхней планкиб меняем вектор скорости на противоположный
+			angle *= dt;
+			if (position.x + radius >= 1280) {
+				velocity.x *= -1;
+				position.x = 1280 - radius;
 
-//            if (Gdx.input.isKeyJustPressed(Input.Keys.A)) {   //move in x -dir, (decrease)
-//                x -= vx * dt;
-//            }
-//            if (Gdx.input.isKeyJustPressed(Input.Keys.W)) {   //move in y-dir, with delta-time binding (increase)
-//                y +=vy * dt;
-//
-//            }
-//        if (Gdx.input.isKeyJustPressed(Input.Keys.S)) {   // move in y direction with  dt -time delta binding (decrease)
-//               y -= vy * dt;
-//            }
+			}
+			if (position.x - radius <= 0) {
+				velocity.x *= -1;
+				position.x = radius;
 
-        }
+			}
+			if (position.y + radius >= 1280) {
+				velocity.y *= -1;
+				position.y = 1280 - radius;
 
-    }
-    Bita bita;
-	Obj obj;
-
-
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-
-		//img2=new Texture("bita.png");
-		bita = new Bita(new Texture("bita.png"),640,-70,700,700);
-		obj = new Obj(new Texture("miyach.png"), 640 ,640, 400, 1900);
-
+			}
+		}
 	}
 
+	Bita bita;
+	Obj obj;
+	private int threshold = -175;
+
 	@Override
-	public void render () {
-	    float dt = Gdx.graphics.getDeltaTime();
+	public void create() {
+		batch = new SpriteBatch();
+
+		bita = new Bita();
+		obj = new Obj();
+
+	}
+	@Override
+	public void render() {
+		float dt = Gdx.graphics.getDeltaTime();
 		update(dt);
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
@@ -106,84 +127,38 @@ public class MyGdxGame extends ApplicationAdapter {
 		batch.end();
 	}
 
-Random rand = new Random();
 	public void update(float dt) {
-      int i = rand.nextInt(2);
-		float r =obj.vy*dt;
-		float rx =obj.vx*dt;
-		if (Gdx.input.isKeyPressed(Input.Keys.D)) {
-		if (Gdx.input.isButtonPressed(Input.Buttons.LEFT)) {
-			bita.angle=125;}
 
-			bita.x+=bita.vx*dt;
+		obj.update(dt);
+		bita.update(dt);
 
-		}
-		if (Gdx.input.isKeyPressed(Input.Keys.A)) {
-			if (Gdx.input.isButtonPressed(Input.Buttons.LEFT)) {
-				bita.angle = 125;
+		 // создаем условия для отбивания мяча от биты с имзменением вектора скорости на противоположную
+
+		if (obj.position.y - obj.radius <= bita.position.y+100
+				&& obj.position.x >= bita.position.x - bita.width
+				&& obj.position.x <= bita.position.x + bita.width) {
+			if (obj.velocity.y < 0) {
+				obj.velocity.y *= -1;
 			}
-			bita.x-=bita.vx*dt;
 		}
+		if (obj.position.y < threshold) {
 
-		obj.y +=(int) obj.vy * dt;          //  движение мяча по игрику
-		obj.x +=(int) obj.vx * dt;          //  движение мяча по иксу
-		obj.angle+=6;
+			// если мяч пролетает мимо биты , выводим сообщение и закрываем приложение
 
-		if ((int)obj.y+r>1280) {    //  верхняя граница
-			obj.y=1280-r;
-			obj.vy *= -1;
-			obj.x+=2;
-
-
-				}
-		if ((int)obj.x+(int)rx>1280) {  // правая граница для мяча
-			obj.x= (int) (1280-rx);
-			obj.vx *= -1;
-			obj.x-=2;
-
-
+			JOptionPane.showMessageDialog(null, "ball is lost!", "You loose!", JOptionPane.INFORMATION_MESSAGE);
+			Gdx.app.exit();
 		}
-		if ((int)obj.x-(int)rx<0) {     // левая граница
-			obj.x=(int)rx;
-			if (i==1) {
-				obj.x+=25;
-				obj.vx *= -1;
-			}
-
-			//obj.vx *= -1;
-
-		}
-
-		if ((int)obj.x<(int)bita.x+301&&(int)obj.x>(int)bita.x-301&&(int)obj.y<(int)bita.y+270) { // условие отбивания от биты( нижняя граница) cast to int  - для более точнного определения границ
-
-			if (i==0){
-
-				obj.vy *= -1;        // изменение направления после отскакивания от биты в случайную сторону посредством рандома
-				obj.vx *= 1;
-
-			}if (i==1) {
-
-				obj.vy *= -1;         // изменение направления после отскакивания от биты в случайную сторону посредством рандома
-				obj.vx *= -1;
-
-			}
-
-		}
-		if (obj.y<-175) {
-			JOptionPane.showMessageDialog(null,"ball is lost!","You loose!",JOptionPane.INFORMATION_MESSAGE);
-			dispose();
-		}
-//
-//	 obj.update(dt);
-//	 bita.update(dt);
 
 	}
-	
+
 	@Override
 	public void dispose () {
 		batch.dispose();
 		obj.texture.dispose();
 		bita.texture.dispose();
-		//img2.dispose();
+
 	}
 }
+
+
+
